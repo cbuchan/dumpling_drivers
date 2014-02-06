08a3eac52effd66b789f8748c0d0d060bdb64af3
Collin Buchan <cbuchan@andrew.cmu.edu>
2014-02-05 20:52:47 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DumplingDrivers::Application.config.secret_key_base = '8d414c4641b96b5054e1414726312d81d85407c4fb98a72b6a476f58c0acb1f856ebddef4f174e93ca6b78c243191c09bb84d16be64b6e726fb9b3787dc2b970'
